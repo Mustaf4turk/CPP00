5e380f0fb7e2590c4724bccdd97184c5d67ee22d
Mustaf4turk <Mustafatu_rk@hotmail.com>
2026-03-11 20:43:28 +0000

    classes added

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,23 +2,20 @@
 #include <string>
 #include <cctype>
 
-std::string to_upper(const std::string& str)
-{
-    std::string result = str;
-    for (size_t i = 0; i < result.length(); i++)
-        result[i] = std::toupper(result[i]);
-    return result;
-}
-
 int main(int argc, char **argv)
 {
-    if (argc > 1)
+    if (argc == 1)
     {
-        for (int i = 1; i < argc; i++)
-            std::cout << to_upper(argv[i]);
-        std::cout << std::endl;
-    }
-    else
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        return 0;
+    }
+    for (int i = 1; i < argc; i++)
+    {
+        std::string str = argv[i];
+        for (size_t j = 0; j < str.length(); j++)
+            str[j] = std::toupper(str[j]);
+        std::cout << str;
+    }
+    std::cout << std::endl;
     return 0;
 }
