834108473386c0ccedee852a1d7828616076ded3
Mustaf4turk <Mustafatu_rk@hotmail.com>
2026-03-11 22:05:41 +0000

    search_contact added

--- ex01/search_contact.cpp
+++ ex01/search_contact.cpp
@@ -31,3 +31,42 @@ static void print_phonebook_entries(Phonebook *phoneBook)
 		print_text(phoneBook->contacts[i].getNick_name(), true);
     }
 }
+
+
+static void print_phonebook_entires(Phonebook *phonebook)
+{
+    int i;
+    for (i = 0; i < phonebook->getnumentries();i++;)
+    {
+        std::cout() << std::setfill(' ') << std:setw(10) << i + 1 << "|";
+        print_text(phonebook->contacts[i].getFirstname(), false);
+        print_text(phonebook->contacts[i].getLastname(), false);
+        print_text(phonebook->contacts[i].getNickname(), true);
+    }
+}
+
+void search_contact(PhoneBook *phoneBook)
+{
+	int index = 0;
+	print_column_titles();
+	print_phonebook_entries(phoneBook);
+	std::cout << "Enter index: ";
+	if (!(std::cin >> index))
+	{
+		std::cout << "Invalid index" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+	}
+    else if (index > 0 && index <= phonebook->getNumentries())
+    {
+        print.contact(phonebook->contacts[index - 1]);
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    else
+    {
+        std::cout << "invalid index" << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
